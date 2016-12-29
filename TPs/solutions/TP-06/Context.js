91a4010227d71cac0d5dd0ca04df7dfe41b8a795
jbcazaux <jbcazaux@gmail.com>
2016-12-29 12:07:06 +0100

    TP-06

---
+++ TPs/solutions/TP-06/Context.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import User from './User';
+
+class Small extends React.Component {
+
+    context;
+
+    static contextTypes = {
+        color: React.PropTypes.string.isRequired,
+        user: React.PropTypes.object.isRequired
+    };
+
+    render() {
+        return <div style={{backgroundColor: this.context.color, height: '125px', width: '33%'}}>
+            Small, user = {this.context.user.id}-{this.context.user.login}
+        </div>;
+    }
+}
+
+class Medium extends React.Component {
+    render() {
+        return <div style={{backgroundColor: 'red', height: '250px', width: '50%'}}>Medium<Small/></div>;
+    }
+}
+
+export default class Large extends React.Component {
+
+    static childContextTypes = {
+        color: React.PropTypes.string.isRequired,
+        user: React.PropTypes.object.isRequired
+    };
+
+    getChildContext() {
+        return {color: 'purple', user: new User(1, 'admin')};
+    }
+
+    render() {
+        return <div style={{backgroundColor: 'blue', color: 'white', height: '300px', width: '75%'}}>
+            Large<Medium/>
+        </div>;
+    }
+}
---
+++ TPs/solutions/TP-06/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Large from "./Context"
+
+ReactDOM.render(
+    <Large/>,
+    document.getElementById("root")
+);

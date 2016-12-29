184b27d260ce6b9d612560740797117c40697bec
jbcazaux <jbcazaux@gmail.com>
2016-12-29 14:46:04 +0100

    TP-07 - optimisation

---
+++ TPs/solutions/TP-07/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Optimisation from "./Optimisation"
+
+ReactDOM.render(
+    <Optimisation/>,
+    document.getElementById("root")
+);

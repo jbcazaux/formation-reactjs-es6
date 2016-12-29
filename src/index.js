c3a139d6fff3a10dd65cc8d8e2f851535a35698f
jbcazaux <jbcazaux@gmail.com>
2016-12-29 11:50:56 +0100

    clean src

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import StudentsApp from "./StudentsApp"
-
-ReactDOM.render(
-    <StudentsApp/>,
-    document.getElementById('root')
-);

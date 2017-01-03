2bae373c5a7d6922ef1bf3d678c4dd1316cd9bf0
jb cazaux <jbcazaux@gmail.com>
2017-01-03 23:06:30 +0100

    react-router TP-09

--- TPs/solutions/TP-09/App.js
+++ TPs/solutions/TP-09/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 class App extends React.Component {
 
     render() {
-        return <div>Hello, React Router!</div>
+        return <div>
+            <h1>Hello, React Router!</h1>
+            <ul>
+                <li><Link to="/part1">Partie 1</Link></li>
+                <li><Link to="/part2">Partie 2</Link></li>
+            </ul>
+            {this.props.children}
+        </div>
     }
 
 }

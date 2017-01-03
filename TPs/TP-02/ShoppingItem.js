39889d95a0abe1a8cfd5d51445b3848bfe9623b8
jbcazaux <jbcazaux@gmail.com>
2017-01-03 13:02:24 +0100

    remove shopping items from TP-02

--- TPs/TP-02/ShoppingItem.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const ShoppingItem = null; // TODO !
-
-export default ShoppingItem;
--- TPs/TP-02/ShoppingList.js
+++ TPs/TP-02/ShoppingList.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import ShoppingItem from "./ShoppingItem"
 
 class ShoppingList extends React.Component {
 

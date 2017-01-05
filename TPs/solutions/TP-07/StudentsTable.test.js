82cd22dae3ea0e7797fdc906da993e4c00fde222
jbcazaux <jbcazaux@gmail.com>
2017-01-05 12:19:42 +0100

    improve TP-07 solution

--- TPs/solutions/TP-07/StudentsTable.test.js
+++ TPs/solutions/TP-07/StudentsTable.test.js
@@ -4,14 +4,22 @@ import StudenstTable from "./StudentsTable";
 import Student from "./Student";
 
 describe(StudenstTable, () => {
-    it('renders studentsTable', () => {
+    it('renders studentsTable with 2 names', () => {
         const onSelectStudent = jest.fn();
         const students = [new Student(1, 'last1', 'firt1', []), new Student(2, 'last2', 'firt2', [])];
         const component = shallow(
             <StudenstTable students={students} selectStudent={onSelectStudent}/>
         );
         expect(component.find('tr')).toHaveLength(3);
-        component.find('tr').at(2).simulate('click');
-        expect(onSelectStudent).toBeCalledWith(students[1]);
+    });
+    it('callbacks with clicked user', () => {
+        const onSelectStudent = jest.fn();
+        let student = new Student(1, 'last1', 'firt1', []);
+        const students = [student];
+        const component = shallow(
+            <StudenstTable students={students} selectStudent={onSelectStudent}/>
+        );
+        component.find('tr').at(1).simulate('click');
+        expect(onSelectStudent).toBeCalledWith(student);
     });
 });

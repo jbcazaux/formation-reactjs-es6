f9dd59e78d81d173a8e761ebd3864464bc34d72a
jb cazaux <jbcazaux@gmail.com>
2017-08-30 20:09:12 +0200

    improve tests of saga

--- TPs/solutions/TP-05-02/sagas/items.js
+++ TPs/solutions/TP-05-02/sagas/items.js
@@ -1,20 +1,32 @@
 import {addItems, getItems, setItems} from '../actions/items';
-import {all, call, put, takeEvery} from "redux-saga/effects";
+import {call, fork, put, take} from "redux-saga/effects";
 import Item from "../Item";
 
-function* loadItems() {
+export function* loadItems() {
     const items = yield call(getItems);
     yield put(setItems(items));
 }
 
-const addItemsWithTVA = function* (action) {
+export const addItemsWithTVA = function* (action) {
     const newItems = action.items.map(item => new Item(item.id, item.label, item.price * 1.20));
     yield put(addItems(newItems));
 };
 
+export function* watchLoadItems() {
+    while (true) {
+        const action = yield take('LOAD_ITEMS');
+        yield call(loadItems, action);
+    }
+}
+
+export function* watchAddItemsWithTVA() {
+    while (true) {
+        const action = yield take('ADD_WITH_TVA');
+        yield call(addItemsWithTVA, action);
+    }
+}
+
 export default function* rootSaga() {
-    yield all([
-        takeEvery('LOAD_ITEMS', loadItems),
-        takeEvery('ADD_WITH_TVA', addItemsWithTVA)
-    ])
+    yield fork(watchLoadItems);
+    yield fork(watchAddItemsWithTVA);
 }
---
+++ TPs/solutions/TP-05-02/sagas/items.test.js
@@ -0,0 +1,32 @@
+import {addItems, getItems, setItems} from '../actions/items';
+import {call, fork, put} from "redux-saga/effects";
+import Item from "../Item";
+import * as sagas from './items';
+import rootSaga from './items';
+
+describe(sagas, () => {
+    it('load items', () => {
+        const gen = sagas.loadItems();
+
+        expect(gen.next().value).toEqual(call(getItems));
+        const items = [];
+        expect(gen.next(items).value).toEqual(put(setItems(items)));
+        expect(gen.next().done).toBeTruthy();
+    });
+
+    it('add items with tva', () => {
+        const action = {items: [new Item(null, null, 1)]};
+        const gen = sagas.addItemsWithTVA(action);
+
+        expect(gen.next().value).toEqual(put(addItems([new Item(null, null, 1.2)])));
+        expect(gen.next().done).toBeTruthy();
+    });
+
+    it('forks watch sagas', () => {
+        const gen = rootSaga();
+
+        expect(gen.next().value).toEqual(fork(sagas.watchLoadItems));
+        expect(gen.next().value).toEqual(fork(sagas.watchAddItemsWithTVA));
+        expect(gen.next().done).toBeTruthy();
+    });
+});

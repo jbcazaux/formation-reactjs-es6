96040a8c1486747300336c91ba2ea2b1410473f2
jb cazaux <jbcazaux@gmail.com>
2017-08-31 20:09:28 +0200

    add tests of sagas

---
+++ TPs/solutions/TP-07-02/sagas/items.js
@@ -0,0 +1,32 @@
+import {addItems, getItems, setItems} from '../actions/items';
+import {call, fork, put, take} from "redux-saga/effects";
+import Item from "../Item";
+
+export function* loadItems() {
+    const items = yield call(getItems);
+    yield put(setItems(items));
+}
+
+export const addItemsWithTVA = function* (action) {
+    const newItems = action.items.map(item => new Item(item.id, item.label, item.price * 1.20));
+    yield put(addItems(newItems));
+};
+
+export function* watchLoadItems() {
+    while (true) {
+        const action = yield take('LOAD_ITEMS');
+        yield call(loadItems, action);
+    }
+}
+
+export function* watchAddItemsWithTVA() {
+    while (true) {
+        const action = yield take('ADD_WITH_TVA');
+        yield call(addItemsWithTVA, action);
+    }
+}
+
+export default function* rootSaga() {
+    yield fork(watchLoadItems);
+    yield fork(watchAddItemsWithTVA);
+}

304f473c83c47d871e8a22d87d680d093d8fae96
jb cazaux <jbcazaux@gmail.com>
2017-09-08 08:26:57 +0200

    add controlled and uncontrolled input demo

---
+++ TPs/demos/controlled/NameForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {firstname: ''};
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        alert('form was submitted: name: ' + this.input.value + ", firstname " + this.state.firstname);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Uncontrolled Name:
+                    <input type="text" ref={(input) => this.input = input} />
+                </label>
+                <label>
+                    Controlled First Name:
+                    <input type="text" onChange={e => this.setState({firstname: e.target.value})} value={this.state.firstname}/>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+export default NameForm;
---
+++ TPs/demos/controlled/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NameForm from "./NameForm";
+
+ReactDOM.render(
+    <NameForm name="cnrs"/>,
+    document.getElementById('root')
+);

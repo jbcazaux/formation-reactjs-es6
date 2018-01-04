6b60872087aefa1a6944f27e8d2ee8785186cbfb
jbcazaux <jbcazaux@gmail.com>
2018-01-04 16:28:57 +0100

    typo: redux dev tools, not react dev tools !

--- TPs/TP-05/index.js
+++ TPs/TP-05/index.js
@@ -5,7 +5,7 @@ import ShoppingList from './ShoppingList';
 import {applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-// uncomment to activate react-dev-tools
+// uncomment to activate redux-dev-tools
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 

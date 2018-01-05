f0fe2335dcd29001b81aa4de75f960554dc873e4
jbcazaux <jbcazaux@gmail.com>
2018-01-05 10:46:32 +0100

    fix: add Enzyme import

--- TPs/TP-07/StudentDetails.test.js
+++ TPs/TP-07/StudentDetails.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {shallow} from "enzyme";
+import Enzyme, {shallow} from "enzyme";
 import StudentDetails from "./StudentDetails";
 import Student from "./Student";
 import Adapter from 'enzyme-adapter-react-16';
@@ -8,6 +8,8 @@ Enzyme.configure({adapter: new Adapter()});
 
 describe('StudentDetails', () => {
     it('renders with no Student', () => {
+        const component = shallow(<StudentDetails student={Student.NULL}/>);
+        expect(component.text()).toContain('Aucun étudiant sélectionné');
     });
     it('renders with a Student', () => {
     });

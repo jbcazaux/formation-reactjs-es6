79402bca127828798f58e6f86b90de294a0c569e
jbcazaux <jbcazaux@gmail.com>
2018-02-08 11:34:12 +0100

    add redux dev tools configuration for TP4

--- TPs/solutions/TP-04/index.js
+++ TPs/solutions/TP-04/index.js
@@ -5,7 +5,8 @@ import {createStore} from 'redux';
 import {reducer} from './reducers/index';
 import ShoppingList from './ShoppingList';
 
-const store = createStore(reducer);
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>

318d6fb7ebc32b195578230cf9a1a8382b066387
jb cazaux <jbcazaux@gmail.com>
2018-11-07 23:07:00 +0100

    add useContext hook

---
+++ TPs/solutions/TP-06/hooks/App.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import Context from './Context'
+import User from './User'
+
+function Small() {
+    const {color, user} = useContext(Context)
+
+    return <div style={{backgroundColor: color, height: '125px', width: '33%'}}>
+        Small, user = {user.id}-{user.login}
+    </div>
+}
+
+class Medium extends React.Component {
+    render() {
+        return <div style={{backgroundColor: 'red', height: '250px', width: '50%'}}>Medium<Small/></div>
+    }
+}
+
+class Large extends React.Component {
+    render() {
+        return <div style={{backgroundColor: 'blue', color: 'white', height: '300px', width: '75%'}}>
+            Large<Medium/>
+        </div>
+    }
+}
+
+export default class App extends React.Component {
+    render() {
+        return <Context.Provider value={{color: 'purple', user: new User(1, 'admin')}}>
+            <Large/>
+        </Context.Provider>
+    }
+}

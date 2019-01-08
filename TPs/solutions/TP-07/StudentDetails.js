5bde488063414ff14c7c06e7d039e52d2c16ddb6
jbcazaux <jbcazaux@gmail.com>
2019-01-08 16:05:42 +0100

    @material-ui/core    3.7.0  →   3.8.2  babel-loader         8.0.4  →   8.0.5  husky                1.2.1  →   1.3.1  webpack-cli          3.1.2  →   3.2.1  webpack-dev-server  3.1.10  →  3.1.14

--- TPs/solutions/TP-07/StudentDetails.js
+++ TPs/solutions/TP-07/StudentDetails.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Student from './Student'
 import FormLabel from '@material-ui/core/FormLabel'
-import { StudentPropTypes } from '../TP-03/Student'
+import { StudentPropTypes } from './Student'
 
 const StudentDetails = ({ student }) => (
   <FormLabel>

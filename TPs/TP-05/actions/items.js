99a8c1a7216b58689100317fbaadf1631ba0da53
jb cazaux <jbcazaux@gmail.com>
2021-10-05 20:24:48 +0200

    typo

--- TPs/TP-05/actions/items.js
+++ TPs/TP-05/actions/items.js
@@ -19,7 +19,7 @@ const getItems = () => axios.get('items.json').then(resp => resp.data)
 export const fetchItems = () => dispatch =>
   getItems()
     .then(items => {
-      //TODO: Commem on a récupéré la liste des items, on peut créer une action grace à l'action creator setItems
+      //TODO: Comme on a récupéré la liste des items, on peut créer une action grace à l'action creator setItems
     })
     .catch(error => {
       console.log(error)

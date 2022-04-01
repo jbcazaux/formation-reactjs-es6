d3a0be7a89c6a7c2e8744ded3bd9e7299d5f7468
jb cazaux <jbcazaux@gmail.com>
2022-04-01 16:25:02 +0200

    fix demo useeffect

--- TPs/demos/useEffect/App.js
+++ TPs/demos/useEffect/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Routes/Home'
 import Route1 from './Routes/Route1'
 import Route2 from './Routes/Route2'
@@ -7,11 +7,11 @@ import Menu from './Menu'
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/route-1" component={Route1} />
-      <Route exact path="/route-2" component={Route2} />
-    </Switch>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/route-1" element={<Route1 />} />
+      <Route exact path="/route-2" element={<Route2 />} />
+    </Routes>
   </BrowserRouter>
 )
 
